7117135d66269f50ab62a2b542f2edfd6338b6e7
99522226 <enkhturtnu@gmail.com>
2026-08-16 10:44:13 +0800

    Update route.ts

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -51,12 +51,7 @@ const processedImageBuffer = await sharp(imageArrayBuffer)
   })
   .toBuffer();
 const imageBlob = new Blob(
-  [
-    processedImageBuffer.buffer.slice(
-      processedImageBuffer.byteOffset,
-      processedImageBuffer.byteOffset + processedImageBuffer.byteLength
-    ),
-  ],
+  [new Uint8Array(processedImageBuffer)],
   {
     type: "image/jpeg",
   }
